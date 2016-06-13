3002195e5b9d34fe3913cd8b1491a4670c61b17f
Vincent Wichmann <vincent.wichmann@gmail.com>
2016-06-13 15:04:25 +0200

    Added localhost url to Program.cs

--- ScrumBoard/src/ScrumBoard/Program.cs
+++ ScrumBoard/src/ScrumBoard/Program.cs
@@ -13,6 +13,7 @@ namespace ScrumBoard
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:80")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
