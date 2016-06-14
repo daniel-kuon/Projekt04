7ce34530ca1c0f0784faafd07e5de47ef85ac109
daniel-kuon <daniel.kuon@outlook.com>
2016-06-14 11:05:43 +0200

    Configured SdDbContext Added Project Model

---
+++ ScrumBoard/src/ScrumBoard/Migrations/SbDbContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ScrumBoard.Models;
+
+namespace ScrumBoard.Migrations
+{
+    [DbContext(typeof(SbDbContext))]
+    partial class SbDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ScrumBoard.Models.Project", b =>
+                {
+                    b.Property<int?>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Deadline");
+
+                    b.Property<string>("Description");
+
+                    b.Property<DateTime>("InsertDate");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("UpdateDate");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Projects");
+                });
+        }
+    }
+}
---
+++ ScrumBoard/src/ScrumBoard/Models/SbDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ScrumBoard.Models
+{
+    /*Schritte für neue Tabelle:
+     * 1. Klasss, die von Entity erbt erstellen
+     * 2. DbSet hier hinzufügen
+     * 3. Klasse in OnModelCreating registrieren
+     * 4. dotnet ef migrations add NAME --context SbDbContext
+     * 5. dotnet ef database update NAME --context SbDbContext
+     *
+    */
+
+    public class SbDbContext:DbContext
+    {
+        public SbDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Project>();
+        }
+    }
+}
