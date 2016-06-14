cba68ef8dd1a60ca1ecbc0954a78b378a9f4546d
Admin <Admin@ADMIN-PC>
2016-06-14 13:29:54 +0200

    Change

---
+++ ScrumBoard/src/ScrumBoard/Controllers/WebApi/JobsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumBoard.Controllers.WebApi
+{
+    [Route("api/jobs")]
+    public class JobsController : ApiController<Job>
+    {
+        public JobsController(SbDbContext context) : base(context)
+        {
+        }
+    }
+}
