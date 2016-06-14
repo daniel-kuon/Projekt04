75e21249f51fd91edf293cec306b606dc2de3b07
Vincent Wichmann <vincent.wichmann@gmail.com>
2016-06-14 11:09:53 +0200

    Added Javascript for index Page

---
+++ ScrumBoard/src/ScrumBoard/TS/index.ts
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $.ajax({
+        type: "POST",
+        url: "/Account/IsLoggedIn",
+        dataType: "json",
+        success: function (msg) {
+            if (msg) { 
+                alert("TEST: Benutzer ist angemeldet!");
+            } else {
+                alert("TEST: Benutzer ist nicht angemeldet!");
+            }
+        }
+    });
+});
