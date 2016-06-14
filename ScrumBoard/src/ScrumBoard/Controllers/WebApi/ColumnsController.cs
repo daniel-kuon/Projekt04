735c624e402d5a5cecb6ef6b355d2fdb6f5efa25
Admin <Admin@ADMIN-PC>
2016-06-14 13:29:41 +0200

    Change

---
+++ ScrumBoard/src/ScrumBoard/Controllers/WebApi/ColumnsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumBoard.Controllers.WebApi
+{
+    [Route("api/columns")]
+    public class ColumnsController : ApiController<Column>
+    {
+        public ColumnsController(SbDbContext context) : base(context)
+        {
+        }
+    }
+}
